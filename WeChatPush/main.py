acd160a129fc3f97a28025e448b0a0ccbc851106
ll2594 <33992739+ll2594@users.noreply.github.com>
2022-07-27 11:40:20 +0800

    添加媒体文件推送
    添加媒体文件(图片，音频，文件，视频)推送，目前还未实现微信电话推送

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -19,7 +19,19 @@ def text_reply(msg):
     urllib3.disable_warnings()
     requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '%s' % (msg.text), 'alias': '%s' % (aliasname)},verify=False)
 
-
+@itchat.msg_register(['Picture', 'Recording', 'Attachment', 'Video'])
+def download_files(msg):
+    urllib3.disable_warnings()
+    #下载媒体文件，服务器硬盘够大可选
+    # msg.download(msg.fileName)
+    typeSymbol = {
+        'Picture': '一张图片',
+        'Recording': '一个音频',
+        'Attachment': '一个文件',
+        'Video': '一个视频', }.get(msg.type, 'fil')
+    print(msg.user.nickName + " 发送了 " + typeSymbol)
+    requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '发送了%s' % (typeSymbol), 'alias': '%s' % (aliasname)},verify=False)
+    
 
 if __name__ == '__main__':
     itchat.check_login()
