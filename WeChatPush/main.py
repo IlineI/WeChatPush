aacafd232eb4e91a3d8a61ceb7dedd222e976968
lilin946 <33992739+ll2594@users.noreply.github.com>
2022-07-30 23:04:40 +0800

    添加推送黑名单功能
    1.规范并精简代码
    2.修复群聊媒体文件不推送
    3.添加推送黑名单功能，可在main.py中修改

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -1,39 +1,45 @@
 import requests
-import itchat
-import itchat.content
-import itchat.config
 from requests.packages import urllib3
+
+import itchat.content
 
 # 你的消息接收别名
 # 例如aliasname = 'line'
-aliasname = ''
-@itchat.msg_register(itchat.content.TEXT)
-def text_reply(msg):
-    print(msg.user.nickName + " say : " + msg.text)
-    urllib3.disable_warnings()
-    requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '%s' % (msg.text), 'alias': '%s' % (aliasname)},verify=False)
+aliasname = '推送别名'
+# 你的消息推动黑名单,可为群名或者好友名，理论上可以按照格式无限添加
+# 例如blacklist = ["相亲相爱一家人","法外狂徒张三","..."]
+blacklist = ["black1", "balck2", "balck3"]
 
 
 @itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
+@itchat.msg_register(itchat.content.TEXT, isFriendChat=True)
 def text_reply(msg):
-    urllib3.disable_warnings()
-    requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '%s' % (msg.text), 'alias': '%s' % (aliasname)},verify=False)
+    if msg.user.nickName not in blacklist:
+        urllib3.disable_warnings()
+        print(msg.user.nickName + msg.text)
+        requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % msg.text,
+                                                       'alias': '%s' % aliasname}, verify=False)
 
-@itchat.msg_register(['Picture', 'Recording', 'Attachment', 'Video'])
+
+@itchat.msg_register([itchat.content.PICTURE, itchat.content.RECORDING, itchat.content.ATTACHMENT,
+                      itchat.content.VIDEO], isGroupChat=True)
+@itchat.msg_register([itchat.content.PICTURE, itchat.content.RECORDING, itchat.content.ATTACHMENT,
+                      itchat.content.VIDEO], isFriendChat=True)
 def download_files(msg):
-    urllib3.disable_warnings()
-    #下载媒体文件，服务器硬盘够大可选
-    # msg.download(msg.fileName)
-    typeSymbol = {
-        'Picture': '一张图片',
-        'Recording': '一个音频',
-        'Attachment': '一个文件',
-        'Video': '一个视频', }.get(msg.type, 'fil')
-    print(msg.user.nickName + " 发送了 " + typeSymbol)
-    requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '发送了%s' % (typeSymbol), 'alias': '%s' % (aliasname)},verify=False)
-    
+    if msg.user.nickName not in blacklist:
+        urllib3.disable_warnings()
+        typesymbol = {
+            itchat.content.PICTURE: '[图片]',
+            itchat.content.RECORDING: '[音频]',
+            itchat.content.ATTACHMENT: '[文件]',
+            itchat.content.VIDEO: '[视频]', }.get(msg.type, 'fil')
+        print(msg.user.nickName + typesymbol)
+        requests.get('https://send.tdtt.top/',
+                     params={'title': '%s' % msg.user.nickName, 'content': '%s' % typesymbol,
+                             'alias': '%s' % aliasname}, verify=False)
+
 
 if __name__ == '__main__':
     itchat.check_login()
-    itchat.auto_login(hotReload=True,enableCmdQR=2)
+    itchat.auto_login(hotReload=True, enableCmdQR=2)
     itchat.run()
