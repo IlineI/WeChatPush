d40345a6f8dc22eb3d450d2b0749e7c9319f665b
line <50483763+IlineI@users.noreply.github.com>
2022-07-31 18:10:55 +0800

    Update main.py

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -4,23 +4,37 @@ import itchat.content
 import itchat.config
 from requests.packages import urllib3
 
+
+# 屏蔽列表
+# 填群聊名称或者微信名称
+# 会不推送这个群聊或者微信的消息
+shield = ['line123', '远方2', 'block3']
+
 # 你的消息接收别名
 # 例如aliasname = 'line'
 aliasname = ''
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-    print(msg.user.nickName + " say : " + msg.text)
+    for title in shield:
+        if msg.user.nickName == title:
+            return
     urllib3.disable_warnings()
     requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '%s' % (msg.text), 'alias': '%s' % (aliasname)},verify=False)
 
 
 @itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
 def text_reply(msg):
+    for title in shield:
+        if msg.user.nickName == title:
+            return
     urllib3.disable_warnings()
     requests.get('https://tdtt.top/send',params={'title': '%s' % (msg.user.nickName), 'content': '%s' % (msg.text), 'alias': '%s' % (aliasname)},verify=False)
 
 @itchat.msg_register(['Picture', 'Recording', 'Attachment', 'Video'])
 def download_files(msg):
+    for title in shield:
+        if msg.user.nickName == title:
+            return
     urllib3.disable_warnings()
     #下载媒体文件，服务器硬盘够大可选
     # msg.download(msg.fileName)
