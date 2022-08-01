f77dc1b6dccd84b514cdcf13b169dd5e49251a64
chase535 <107602367+chase535@users.noreply.github.com>
2022-08-01 17:10:04 +0800

    Update main.py

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -1,7 +1,8 @@
 import requests
 from requests.packages import urllib3
-import itchat.content
+from datetime import datetime
 import re
+import itchat.content
 
 # 你的消息接收别名
 # 例如aliasname = 'line'
@@ -9,48 +10,45 @@ aliasname = '推送别名'
 # 你的消息推送黑名单,可为群名或者好友名，理论上可以按照格式无限添加
 # 例如blacklist = ["相亲相爱一家人","法外狂徒张三","..."]
 blacklist = ["black1", "balck2", "balck3"]
+#消息推送接口
+#旧接口，腾讯云，服务器在北京，延时低，无推送量限制，2023年2月起失效
+interface = 'https://tdtt.top/send'
+#新接口，Cloudflare平台，服务器在美国，延时高，日总推送量5万条，理论永久有效
+#interface = 'https://send.tdtt.top'
+
+urllib3.disable_warnings()
+
+@itchat.msg_register([itchat.content.TEXT, itchat.content.FRIENDS, itchat.content.MUSICSHARE, itchat.content.UNDEFINED, itchat.content.PICTURE,
+                        itchat.content.RECORDING, itchat.content.ATTACHMENT, itchat.content.VIDEO, itchat.content.CARD, itchat.content.EMOTICON,
+                        itchat.content.VOIP, itchat.content.WEBSHARE, itchat.content.LOCATIONSHARE, itchat.content.CHATHISTORY, itchat.content.SHARING,
+                        itchat.content.TRANSFER, itchat.content.REDENVELOPE, itchat.content.MINIPROGRAM, itchat.content.MAP], isFriendChat=True, isGroupChat=True)
 
 
-@itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
-@itchat.msg_register(itchat.content.TEXT, isFriendChat=True)
-def text_reply(msg):
-    if msg.user.nickName not in blacklist:
-        urllib3.disable_warnings()
-        print(msg.user.nickName + msg.text)
-        requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % msg.text, 'alias': '%s' % aliasname}, verify=False)
-
-@itchat.msg_register([itchat.content.PICTURE, itchat.content.RECORDING, itchat.content.ATTACHMENT, itchat.content.VIDEO], isGroupChat=True)
-@itchat.msg_register([itchat.content.PICTURE, itchat.content.RECORDING, itchat.content.ATTACHMENT, itchat.content.VIDEO], isFriendChat=True)
-def download_files(msg):
-    if msg.user.nickName not in blacklist:
-        urllib3.disable_warnings()
+def simple_reply(msg):
+    name = msg.user.nickName if msg.user.remarkName == '' else msg.user.remarkName
+    if name not in blacklist:
         typesymbol = {
+            itchat.content.TEXT: str(msg.get('Text')),
+            itchat.content.FRIENDS: '好友请求',
             itchat.content.PICTURE: '[图片]',
-            itchat.content.RECORDING: '[音频]',
-            itchat.content.ATTACHMENT: '[文件]',
-            itchat.content.VIDEO: '[视频]', }.get(msg.type, 'fil')
-        print(msg.user.nickName + typesymbol)
-        requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % typesymbol, 'alias': '%s' % aliasname}, verify=False)
-
-@itchat.msg_register([itchat.content.MAP, itchat.content.SHARING, itchat.content.CARD], isGroupChat=True)
-@itchat.msg_register([itchat.content.MAP, itchat.content.SHARING, itchat.content.CARD], isFriendChat=True)
-def receive_msg(msg):
-    if msg.user.nickName not in blacklist:
-        if msg['Type'] == 'Card':
-            urllib3.disable_warnings()
-            print(msg['RecommendInfo']['NickName'])
-            requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '向你推荐了：%s' % msg['RecommendInfo']['NickName'], 'alias': '%s' % aliasname}, verify=False)
-        elif msg['Type'] == 'Map':
-            urllib3.disable_warnings()
-            x, y, location = re.search("<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(1, 2, 3)
-            locationname = '地址：' + location + '\n经纬度：' + x + ',' + y
-            print(locationname)
-            requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % locationname, 'alias': '%s' % aliasname}, verify=False)
-        elif msg['Type'] == 'Sharing':
-            urllib3.disable_warnings()
-            Sharing = '分享：' + msg['Text'] + '\n链接：' + msg['Url']
-            print(Sharing)
-            requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % Sharing, 'alias': '%s' % aliasname}, verify=False)
+            itchat.content.RECORDING: '[语音]',
+            itchat.content.VIDEO: '[视频]',
+            itchat.content.LOCATIONSHARE: '[共享实时位置]',
+            itchat.content.CHATHISTORY: '[聊天记录]',
+            itchat.content.TRANSFER: '[转账]',
+            itchat.content.REDENVELOPE: '[红包]',
+            itchat.content.EMOTICON: '[动画表情]',
+            itchat.content.VOIP: '[通话邀请] 请及时打开微信',
+            itchat.content.ATTACHMENT: '[文件]' + str(msg.get('Text')),
+            itchat.content.CARD: '[好友名片]' + str(msg.get('Text')),
+            itchat.content.MUSICSHARE: '[音乐]' + str(msg.get('Text')),
+            itchat.content.MAP: '[位置分享]' + str(''.join(re.findall(r'poiname="(.*?)" poiid', str(msg.get('OriContent'))))),
+            itchat.content.WEBSHARE: '[链接]' + str(msg.get('Text')),
+            itchat.content.MINIPROGRAM: '[小程序]' + str(msg.get('Text')),
+            itchat.content.SHARING: '[未知卡片消息]: AppMsgType=' + str(msg.get('Text')),
+            itchat.content.UNDEFINED: '[未知消息类型]: MsgType=' + str(msg.get('Text')) }.get(msg['Type'])
+        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + str(name) + ': ' + str(typesymbol))
+        requests.get(str(interface), params={'title': '微信 ' + str(name), 'content': str(typesymbol), 'alias': str(aliasname)}, verify=False)
 
 if __name__ == '__main__':
     itchat.check_login()
