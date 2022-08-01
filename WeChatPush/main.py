4835b67f426ce1ba834ade91268f5d2b28c3650c
chase535 <107602367+chase535@users.noreply.github.com>
2022-08-01 21:22:04 +0800

    Update main.py

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -4,12 +4,12 @@ from datetime import datetime
 import re
 import itchat.content
 
-# 你的消息接收别名
+# 你的消息推送别名
 # 例如aliasname = 'line'
 aliasname = '推送别名'
-# 你的消息推送黑名单,可为群名或者好友名，理论上可以按照格式无限添加
+# 你的消息推送黑名单,可以是好友昵称或原群名，不是好友备注或群备注，理论上可以按照格式无限添加
 # 例如blacklist = ["相亲相爱一家人","法外狂徒张三","..."]
-blacklist = ["black1", "balck2", "balck3"]
+blacklist = ['blacklist']
 #消息推送接口
 #旧接口，腾讯云，服务器在北京，延时低，无推送量限制，2023年2月起失效
 interface = 'https://tdtt.top/send'
@@ -26,7 +26,7 @@ urllib3.disable_warnings()
 
 def simple_reply(msg):
     name = msg.user.nickName if msg.user.remarkName == '' else msg.user.remarkName
-    if name not in blacklist:
+    if msg.user.nickName not in blacklist:
         typesymbol = {
             itchat.content.TEXT: str(msg.get('Text')),
             itchat.content.FRIENDS: '好友请求',
@@ -38,16 +38,16 @@ def simple_reply(msg):
             itchat.content.TRANSFER: '[转账]',
             itchat.content.REDENVELOPE: '[红包]',
             itchat.content.EMOTICON: '[动画表情]',
-            itchat.content.VOIP: '[通话邀请] 请及时打开微信',
+            itchat.content.VOIP: '[通话邀请]请及时打开微信',
             itchat.content.ATTACHMENT: '[文件]' + str(msg.get('Text')),
-            itchat.content.CARD: '[好友名片]' + str(msg.get('Text')),
+            itchat.content.CARD: '[名片]' + str(msg.get('Text')),
             itchat.content.MUSICSHARE: '[音乐]' + str(msg.get('Text')),
             itchat.content.MAP: '[位置分享]' + str(''.join(re.findall(r'poiname="(.*?)" poiid', str(msg.get('OriContent'))))),
             itchat.content.WEBSHARE: '[链接]' + str(msg.get('Text')),
             itchat.content.MINIPROGRAM: '[小程序]' + str(msg.get('Text')),
             itchat.content.SHARING: '[未知卡片消息]: AppMsgType=' + str(msg.get('Text')),
             itchat.content.UNDEFINED: '[未知消息类型]: MsgType=' + str(msg.get('Text')) }.get(msg['Type'])
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + str(name) + ': ' + str(typesymbol))
+        print(datetime.now().strftime('%Y.%m.%d %H:%M:%S') + ' ' + str(name) + ': ' + str(typesymbol))
         requests.get(str(interface), params={'title': '微信 ' + str(name), 'content': str(typesymbol), 'alias': str(aliasname)}, verify=False)
 
 if __name__ == '__main__':
