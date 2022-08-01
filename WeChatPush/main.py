7f7760f2cc402dc328b16a5315a8a933d77083ed
line <50483763+IlineI@users.noreply.github.com>
2022-08-01 10:35:53 +0800

    Update main.py

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -1,6 +1,8 @@
 import requests
 from requests.packages import urllib3
 import itchat.content
+import re
+
 # 你的消息接收别名
 # 例如aliasname = 'line'
 aliasname = '推送别名'
@@ -29,6 +31,26 @@ def download_files(msg):
             itchat.content.VIDEO: '[视频]', }.get(msg.type, 'fil')
         print(msg.user.nickName + typesymbol)
         requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % typesymbol, 'alias': '%s' % aliasname}, verify=False)
+
+@itchat.msg_register([itchat.content.MAP, itchat.content.SHARING, itchat.content.CARD], isGroupChat=True)
+@itchat.msg_register([itchat.content.MAP, itchat.content.SHARING, itchat.content.CARD], isFriendChat=True)
+def receive_msg(msg):
+    if msg.user.nickName not in blacklist:
+        if msg['Type'] == 'Card':
+            urllib3.disable_warnings()
+            print(msg['RecommendInfo']['NickName'])
+            requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '向你推荐了：%s' % msg['RecommendInfo']['NickName'], 'alias': '%s' % aliasname}, verify=False)
+        elif msg['Type'] == 'Map':
+            urllib3.disable_warnings()
+            x, y, location = re.search("<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(1, 2, 3)
+            locationname = '地址：' + location + '\n经纬度：' + x + ',' + y
+            print(locationname)
+            requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % locationname, 'alias': '%s' % aliasname}, verify=False)
+        elif msg['Type'] == 'Sharing':
+            urllib3.disable_warnings()
+            Sharing = '分享：' + msg['Text'] + '\n链接：' + msg['Url']
+            print(Sharing)
+            requests.get('https://send.tdtt.top/', params={'title': '%s' % msg.user.nickName, 'content': '%s' % Sharing, 'alias': '%s' % aliasname}, verify=False)
 
 if __name__ == '__main__':
     itchat.check_login()
