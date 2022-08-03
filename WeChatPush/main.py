c3d4a1481e6e0c31c9498b9042e190d06ec18851
chase535 <107602367+chase535@users.noreply.github.com>
2022-08-03 13:41:32 +0800

    Update main.py

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -4,28 +4,6 @@ import re
 from requests.packages import urllib3
 from datetime import datetime
 
-# 是否将消息通知与通话邀请分开推送
-# 若此项不为false，则需设置两个别名，一个用来接收消息通知，一个用来接收通话邀请
-# 好处是可以双开，将接收通话邀请的那个的通知声设置为电话铃声，起到强提醒的作用
-# 注意，一个app的mipush信息仅可设置一个通知声，所以需要双开才能设置两个不同的通知声
-separate_push = 'false'
-
-# 消息推送别名
-chat_alias = ''
-# 通话邀请推送别名，若separate_push为false则无需填写
-# 若separate_push不为false而此项未填写，则强制使用消息推送的别名
-VoIP_alias = ''
-
-# 消息推送黑名单,可以是好友昵称或原群名，不是好友备注或群备注，理论上可以按照格式无限添加
-# 例如blacklist = ['相亲相爱一家人', '法外狂徒张三', '...']
-blacklist = ['blacklist']
-
-# 消息推送接口
-# 旧接口，腾讯云，服务器在北京，延时低，无推送量限制，2023年2月起失效
-interface = 'https://tdtt.top/send'
-# 新接口，Cloudflare平台，服务器在美国，延时高，日总推送量5万条，理论永久有效
-#interface = 'https://send.tdtt.top'
-
 urllib3.disable_warnings()
 
 @itchat.msg_register([itchat.content.TEXT, itchat.content.FRIENDS, itchat.content.MUSICSHARE,
@@ -38,6 +16,19 @@ urllib3.disable_warnings()
 
 
 def simple_reply(msg):
+    config = open('./config.txt', 'r', encoding='utf-8')
+    for line in config:
+        if (re.findall(r'^separate_push = \'(.*?)\'', line)) != []:
+            separate_push = str(''.join(re.findall(r'^separate_push = \'(.*?)\'', line)))
+        elif (re.findall(r'^chat_alias = \'(.*?)\'', line)) != []:
+            chat_alias = str(''.join(re.findall(r'^chat_alias = \'(.*?)\'', line)))
+        elif (re.findall(r'^VoIP_alias = \'(.*?)\'', line)) != []:
+            VoIP_alias = str(''.join(re.findall(r'^VoIP_alias = \'(.*?)\'', line)))
+        elif (''.join(re.findall(r'^blacklist = \[(.*?)\]', line))) != '':
+            blacklist = '[' + str(''.join(re.findall(r'^blacklist = \[(.*?)\]', line))) + ']'
+        elif (re.findall(r'^interface = \'(.*?)\'', line)) != []:
+            interface = str(''.join(re.findall(r'^interface = \'(.*?)\'', line)))
+    config.close()
     if msg.get('NickName') not in blacklist:
         typesymbol = {
             itchat.content.TEXT: str(msg.get('Text')),
@@ -61,7 +52,7 @@ def simple_reply(msg):
             itchat.content.MINIPROGRAM: '[小程序]' + str(msg.get('Text')),
             itchat.content.UNDEFINED: '[未知消息类型]: MsgType=' + str(msg.get('Text')) }.get(msg['Type'])
         voip_alias = VoIP_alias if separate_push != 'false' and VoIP_alias != '' else chat_alias
-        alias = voip_alias if msg.get('Type') == 'Voip' else chat_alias
+        alias = voip_alias if msg.get('Type') == 'Text' else chat_alias
         requests.get(str(interface), params={'title': '微信 ' + str(msg.get('Name')), 'content': str(typesymbol), 'alias': str(alias)}, verify=False)
         typesymbol = '[未知卡片消息]: AppMsgType=' + str(msg.get('Text')) if msg.get('Type') == 'Sharing' else typesymbol
         print(datetime.now().strftime('%Y.%m.%d %H:%M:%S') + ' ' + str(msg.get('Name')) + ': ' + str(typesymbol))
