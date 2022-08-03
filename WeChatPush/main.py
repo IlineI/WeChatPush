58e75546ff379d44ca87b06e2436c5d9fc09d61b
chase535 <107602367+chase535@users.noreply.github.com>
2022-08-03 14:23:58 +0800

    Update main.py

--- WeChatPush/main.py
+++ WeChatPush/main.py
@@ -16,7 +16,7 @@ urllib3.disable_warnings()
 
 
 def simple_reply(msg):
-    config = open('./config.txt', 'r', encoding='utf-8')
+    config = open(os.path.split(os.path.realpath(__file__))[0] + '/config.txt', 'r', encoding='utf-8')
     for line in config:
         if (re.findall(r'^separate_push = \'(.*?)\'', line)) != []:
             separate_push = str(''.join(re.findall(r'^separate_push = \'(.*?)\'', line)))
@@ -52,7 +52,7 @@ def simple_reply(msg):
             itchat.content.MINIPROGRAM: '[小程序]' + str(msg.get('Text')),
             itchat.content.UNDEFINED: '[未知消息类型]: MsgType=' + str(msg.get('Text')) }.get(msg['Type'])
         voip_alias = VoIP_alias if separate_push != 'false' and VoIP_alias != '' else chat_alias
-        alias = voip_alias if msg.get('Type') == 'Text' else chat_alias
+        alias = voip_alias if msg.get('Type') == 'Voip' else chat_alias
         requests.get(str(interface), params={'title': '微信 ' + str(msg.get('Name')), 'content': str(typesymbol), 'alias': str(alias)}, verify=False)
         typesymbol = '[未知卡片消息]: AppMsgType=' + str(msg.get('Text')) if msg.get('Type') == 'Sharing' else typesymbol
         print(datetime.now().strftime('%Y.%m.%d %H:%M:%S') + ' ' + str(msg.get('Name')) + ': ' + str(typesymbol))
