e8a3b311c3efd8e2a7159c2729f2a5ebdd93a49d
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2022-08-24 19:26:39 +0800

    Update register.py

--- WeChatPush/itchat/components/register.py
+++ WeChatPush/itchat/components/register.py
@@ -1,5 +1,5 @@
 import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+sys.setdefaultencoding("utf-8")
 import logging, traceback, threading
 import os
 from datetime import datetime
