0243169f5a9c86025b16e71727875638fac4ddeb
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2022-08-24 18:58:07 +0800

    Update __init__.py

--- WeChatPush/itchat/__init__.py
+++ WeChatPush/itchat/__init__.py
@@ -1,5 +1,5 @@
 import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="urf-8")
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 import os
 from .core import Core
 from .config import VERSION
